cdb6e91de03fc0bd45388424a6df4d6e26c9affb
Peter Werner <pcw1995@yahoo.com>
2019-09-16 20:23:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,6 +8,7 @@ def load_library(file)
   hash[:get_meaning] ={}
   hash[:get_emoticon]={}
   emoji.each_key do |key|
+    p key 
     hash[:get_meaning]={"#{emoji[key][1]}" => "#{key}"}
     hash[:get_emoticon] ={"#{emoji[key][0]}" => "#{emoji[key][1]}"}
   end 
