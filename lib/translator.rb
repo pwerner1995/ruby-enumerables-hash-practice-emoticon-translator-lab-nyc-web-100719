4c018bb4c967f7375875711f0a22fcc03226712f
Peter Werner <pcw1995@yahoo.com>
2019-09-16 20:28:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(file)
   hash[:get_emoticon]={}
   emoji.each_key do |key|
     p emoji[key][0]
-    hash[:get_meaning]={"#{emoji[key][1]}" => "#{key}"}
+    hash[:get_meaning]["#{emoji[key][1]}"] = "#{key}"
     hash[:get_emoticon] ={"#{emoji[key][0]}" => "#{emoji[key][1]}"}
   end 
   #p emoji
